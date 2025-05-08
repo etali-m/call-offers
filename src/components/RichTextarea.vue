c960e4b6853d66ab170d1b2175ebab869888a359
etali-m <etalijunior15@gmail.com>
2025-05-08 08:11:00 +0100

    toggle table options

--- src/components/RichTextarea.vue
+++ src/components/RichTextarea.vue
@@ -17,66 +17,72 @@
       <button type="button" @click="clearFormatting">
         <EraserIcon />
       </button>
+      <button type="button" @click="toggleTable">
+        Tableau
+      </button>
 
-      <!-- Boutons de tableau -->
-      <button @click="editor.chain().focus().insertTable({ rows: 3, cols: 3, withHeaderRow: true }).run()">
-        Insert table
-      </button>
-      <button @click="editor.chain().focus().insertContent(tableHTML, { parseOptions: { preserveWhitespace: false }}).run()">
-        Insert HTML table
-      </button>
-      <button @click="editor.chain().focus().addColumnBefore().run()" :disabled="!editor.can().addColumnBefore()">
-        Add column before
-      </button>
-      <button @click="editor.chain().focus().addColumnAfter().run()" :disabled="!editor.can().addColumnAfter()">
-        Add column after
-      </button>
-      <button @click="editor.chain().focus().deleteColumn().run()" :disabled="!editor.can().deleteColumn()">
-        Delete column
-      </button>
-      <button @click="editor.chain().focus().addRowBefore().run()" :disabled="!editor.can().addRowBefore()">
-        Add row before
-      </button>
-      <button @click="editor.chain().focus().addRowAfter().run()" :disabled="!editor.can().addRowAfter()">
-        Add row after
-      </button>
-      <button @click="editor.chain().focus().deleteRow().run()" :disabled="!editor.can().deleteRow()">
-        Delete row
-      </button>
-      <button @click="editor.chain().focus().deleteTable().run()" :disabled="!editor.can().deleteTable()">
-        Delete table
-      </button>
-      <button @click="editor.chain().focus().mergeCells().run()" :disabled="!editor.can().mergeCells()">
-        Merge cells
-      </button>
-      <button @click="editor.chain().focus().splitCell().run()" :disabled="!editor.can().splitCell()">
-        Split cell
-      </button>
-      <button @click="editor.chain().focus().toggleHeaderColumn().run()" :disabled="!editor.can().toggleHeaderColumn()">
-        Toggle header column
-      </button>
-      <button @click="editor.chain().focus().toggleHeaderRow().run()" :disabled="!editor.can().toggleHeaderRow()">
-        Toggle header row
-      </button>
-      <button @click="editor.chain().focus().toggleHeaderCell().run()" :disabled="!editor.can().toggleHeaderCell()">
-        Toggle header cell
-      </button>
-      <button @click="editor.chain().focus().mergeOrSplit().run()" :disabled="!editor.can().mergeOrSplit()">
-        Merge or split
-      </button>
-      <button @click="editor.chain().focus().setCellAttribute('backgroundColor', '#FAF594').run()" 
-        :disabled="!editor.can().setCellAttribute('backgroundColor', '#FAF594')">
-        Set cell attribute
-      </button>
-      <button @click="editor.chain().focus().fixTables().run()" :disabled="!editor.can().fixTables()">
-        Fix tables
-      </button>
-      <button @click="editor.chain().focus().goToNextCell().run()" :disabled="!editor.can().goToNextCell()">
-        Go to next cell
-      </button>
-      <button @click="editor.chain().focus().goToPreviousCell().run()" :disabled="!editor.can().goToPreviousCell()">
-        Go to previous cell
-      </button>
+      <div v-if="showTable" class="toolbar">
+         <!-- Boutons de tableau -->
+        <button type="button" @click="editor.chain().focus().insertTable({ rows: 3, cols: 3, withHeaderRow: true }).run()">
+          Insert table
+        </button>
+        <button type="button" @click="editor.chain().focus().insertContent(tableHTML, { parseOptions: { preserveWhitespace: false }}).run()">
+          Insert HTML table
+        </button>
+        <button type="button" @click="editor.chain().focus().addColumnBefore().run()" :disabled="!editor.can().addColumnBefore()">
+          Add column before
+        </button>
+        <button type="button" @click="editor.chain().focus().addColumnAfter().run()" :disabled="!editor.can().addColumnAfter()">
+          Add column after
+        </button>
+        <button type="button" @click="editor.chain().focus().deleteColumn().run()" :disabled="!editor.can().deleteColumn()">
+          Delete column
+        </button>
+        <button type="button" @click="editor.chain().focus().addRowBefore().run()" :disabled="!editor.can().addRowBefore()">
+          Add row before
+        </button>
+        <button type="button" @click="editor.chain().focus().addRowAfter().run()" :disabled="!editor.can().addRowAfter()">
+          Add row after
+        </button>
+        <button type="button" @click="editor.chain().focus().deleteRow().run()" :disabled="!editor.can().deleteRow()">
+          Delete row
+        </button>
+        <button type="button" @click="editor.chain().focus().deleteTable().run()" :disabled="!editor.can().deleteTable()">
+          Delete table
+        </button>
+        <button type="button" @click="editor.chain().focus().mergeCells().run()" :disabled="!editor.can().mergeCells()">
+          Merge cells
+        </button>
+        <button type="button" @click="editor.chain().focus().splitCell().run()" :disabled="!editor.can().splitCell()">
+          Split cell
+        </button>
+        <button type="button" @click="editor.chain().focus().toggleHeaderColumn().run()" :disabled="!editor.can().toggleHeaderColumn()">
+          Toggle header column
+        </button>
+        <button type="button" @click="editor.chain().focus().toggleHeaderRow().run()" :disabled="!editor.can().toggleHeaderRow()">
+          Toggle header row
+        </button>
+        <button type="button" @click="editor.chain().focus().toggleHeaderCell().run()" :disabled="!editor.can().toggleHeaderCell()">
+          Toggle header cell
+        </button>
+        <button type="button" @click="editor.chain().focus().mergeOrSplit().run()" :disabled="!editor.can().mergeOrSplit()">
+          Merge or split
+        </button>
+        <button type="button" @click="editor.chain().focus().setCellAttribute('backgroundColor', '#FAF594').run()" 
+          :disabled="!editor.can().setCellAttribute('backgroundColor', '#FAF594')">
+          Set cell attribute
+        </button>
+        <button type="button" @click="editor.chain().focus().fixTables().run()" :disabled="!editor.can().fixTables()">
+          Fix tables
+        </button>
+        <button type="button" @click="editor.chain().focus().goToNextCell().run()" :disabled="!editor.can().goToNextCell()">
+          Go to next cell
+        </button>
+        <button type="button" @click="editor.chain().focus().goToPreviousCell().run()" :disabled="!editor.can().goToPreviousCell()">
+          Go to previous cell
+        </button>
+      </div>
+
     </div>
     <EditorContent :editor="editor" class="editor" />
   </div>
@@ -89,12 +95,18 @@ import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
-import { onBeforeUnmount, watch } from 'vue'
+import { onBeforeUnmount, watch, ref } from 'vue'
 import { ListIcon, ListOrderedIcon, BoldIcon, ItalicIcon, EraserIcon } from 'lucide-vue-next'
 
 const props = defineProps({
   modelValue: String
 })
+
+const showTable = ref(false)
+
+const toggleTable = () => {
+  showTable.value = !showTable.value;
+}
 
 const emit = defineEmits(['update:modelValue'])
 
